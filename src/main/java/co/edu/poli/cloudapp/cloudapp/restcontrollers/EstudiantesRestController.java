a9a55da53a2944e4e6bfb6ce9f3dc92c2a34fc5d
Vilez0 <samueldavidmolina2007@gmail.com>
2025-11-11 10:28:43 -0500

    Web service con Spring Boot

---
+++ src/main/java/co/edu/poli/cloudapp/cloudapp/restcontrollers/EstudiantesRestController.java
@@ -0,0 +1,25 @@
+package co.edu.poli.cloudapp.cloudapp.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.poli.cloudapp.cloudapp.dto.EstudianteDTO;
+import co.edu.poli.cloudapp.cloudapp.services.IEstudianteService;
+
+@RestController
+@RequestMapping("/api/estudiantes")
+public class EstudiantesRestController {
+    @Autowired
+    private IEstudianteService estudianteService;
+
+    @GetMapping
+    public ResponseEntity<List<EstudianteDTO>> getAll() {
+        return ResponseEntity.ok(estudianteService.finAll());
+    }
+    
+}
